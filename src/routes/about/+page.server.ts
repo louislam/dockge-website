286047a2017869828b362f09077dcf6176d2a42d
Louis Lam <louislam@users.noreply.github.com>
2023-11-24 21:14:04 +0800

    WIP

---
+++ src/routes/about/+page.server.ts
@@ -0,0 +1,7 @@
+export async function load() {
+    return {
+        title: "About",
+        content: "This is the about page23"
+    }
+}
+
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let data;
+</script>
+
+<h1>{data.title}</h1>
+<div>{data.content}</div>
+
